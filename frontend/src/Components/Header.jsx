fa78122d0505e33d05123b4aea2c80f689eb024e
Gaurav singh bisht <gauravbisht82754@gmail.com>
2024-05-10 04:02:37 +0530

    second change

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -24,7 +24,7 @@ const Header = ({ item }) => {
                 <a href="/">Homepage</a>
               </li>
               <li>
-                <a href="/Contact">Contact</a>
+                <a href="/contact">Contact</a>
               </li>
               <li>
                 <a href="/about">About</a>
